ed4cf53bc345f1350db373d7bb470f7824b7927c
iRE-X <harekrishna8016@gmail.com>
2023-02-13 21:56:41 +0530

    Feb 13

--- client/src/components/review/Review.js
+++ client/src/components/review/Review.js
@@ -1,6 +1,6 @@
-import getUser from "../userRequests/getUser";
-
 import { useState, useEffect } from "react";
+import getUser from "../userRequests/getUser";
+import _ from "lodash";
 
 const Review = ({ review }) => {
     // const user = await getUser(review.user);
@@ -13,7 +13,8 @@ const Review = ({ review }) => {
         getDonor();
     }, [review.user]);
 
-    // let starCount = review.rating;
+    let stars = _.range(review.rating);
+    const emptyStars = _.range(5 - review.rating);
 
     return (
         <div className="swiper-slide box">
@@ -28,12 +29,12 @@ const Review = ({ review }) => {
             <h3>{user ? user.name : "Unknown"}</h3>
             <p>{review.content}</p>
             <div className="stars">
-                {/* {starCount * <i className="fas fa-star"></i>} */}
-
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star-half-alt"></i>
+                {stars.map(ele => (
+                    <i key={ele} className="fa fa-star"></i>
+                ))}
+                {emptyStars.map(ele => (
+                    <i key={ele} className="fa fa-star-o"></i>
+                ))}
             </div>
         </div>
     );
