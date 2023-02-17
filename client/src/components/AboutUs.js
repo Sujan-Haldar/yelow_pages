07cd376aa5fe9bf16a44051abc4bf0c96d21e584
iRE-X <harekrishna8016@gmail.com>
2023-02-17 12:44:20 +0530

    about us

--- client/src/components/AboutUs.js
+++ client/src/components/AboutUs.js
@@ -6,12 +6,12 @@ import sujanPic from "../image/sujan.jpg";
 import sunandanPic from "../image/sunandan.jpg";
 import Member from "./about_us/Member";
 function AboutUs() {
-    const dept = 'Computer Science and Engineering';
-    const college = 'Techno International Batanagar';
-    function member(name,dept,college,mobile,email,fbLink,ldLink,image){
+    const dept = "Computer Science and Engineering";
+    const college = "Techno International Batanagar";
+    function member(name, dept, college, mobile, email, fbLink, ldLink, image) {
         this.name = name;
         this.dept = dept;
-        this.college= college;
+        this.college = college;
         this.mobile = mobile;
         this.email = email;
         this.fbLink = fbLink;
@@ -27,8 +27,7 @@ function AboutUs() {
         "https://www.facebook.com/ardhendu.dhua.1",
         "https://www.linkedin.com/in/ardhendu-dhua-ab7626247/",
         ardhenduPic
-
-    )
+    );
     const sunandan = new member(
         "Sunandan Ghosh",
         dept,
@@ -38,7 +37,7 @@ function AboutUs() {
         "https://www.facebook.com/sunandan.ghosh.98",
         "https://www.linkedin.com/in/sunandan-ghosh-609285220/",
         sunandanPic
-    )
+    );
     const harekrishna = new member(
         "Harekrishna Jana",
         dept,
@@ -48,7 +47,7 @@ function AboutUs() {
         "https://www.facebook.com/harekrishna.jana.9655",
         "https://www.linkedin.com/in/harekrishna-jana-481126217/",
         harekrishnaPic
-    )
+    );
     const bhairab = new member(
         "Bhairab Roy",
         dept,
@@ -58,7 +57,7 @@ function AboutUs() {
         "https://www.facebook.com/virab.roy",
         "https://www.linkedin.com/in/bhairab-roy-363606246/",
         bhairabPic
-    )
+    );
     const sujan = new member(
         "Sujan Haldar",
         dept,
@@ -68,27 +67,41 @@ function AboutUs() {
         "https://www.facebook.com/sujanhaldara1",
         "https://www.linkedin.com/in/sujan-haldar-493303234/",
         sujanPic
-    )
+    );
 
-    return ( 
+    return (
         <section class="aboutus" id="aboutus">
+            <h1 class="heading1">
+                <span>About Us</span>
+            </h1>
+            <p className="content">
+                Our project's objective is to create a website where people can
+                easily donate their gently used or new books which they don't
+                need in future and where they can also collect there necessary
+                books if available. <br />
+                <br />
+                We aim to promote
+                <ul>
+                    <li>literacy and love of reading.</li>
+                    <li>Help the Environment by Recycling.</li>
+                    <li>Sharing favourite stories with others.</li>
+                </ul>
+            </p>
+            <h1 class="heading1">
+                <span>Meet Our Team Members</span>{" "}
+            </h1>
 
-        <h1 class="heading1"> <span>About Us</span> </h1>
-
-        <div class="aboutus-container">
-
-            <div class="aboutus-container-wrapper">
-                <Member memberDetails = {harekrishna}/>
-                <Member memberDetails = {sujan}/>
-                <Member memberDetails = {sunandan}/>
-                <Member memberDetails = {bhairab}/>
-                <Member memberDetails = {ardhendu}/>
-                
+            <div class="aboutus-container">
+                <div class="aboutus-container-wrapper">
+                    <Member memberDetails={harekrishna} />
+                    <Member memberDetails={sujan} />
+                    <Member memberDetails={sunandan} />
+                    <Member memberDetails={bhairab} />
+                    <Member memberDetails={ardhendu} />
+                </div>
             </div>
-        </div>
-
-    </section>
-     );
+        </section>
+    );
 }
 
 export default AboutUs;
--- client/src/components/about_us/Member.js
+++ client/src/components/about_us/Member.js
@@ -1,33 +1,34 @@
 import faceboookImg from "../../image/facebook.png";
 import linkdinImg from "../../image/linkedin.png";
-function Member({memberDetails}) {
-    const {name,dept,college,mobile,email,fbLink,ldLink,image} = memberDetails;
-    return ( 
+function Member({ memberDetails }) {
+    const { name, dept, college, mobile, email, fbLink, ldLink, image } =
+        memberDetails;
+    return (
         <div class="aboutus box">
-                    <div class="content">
-                        <div class="aboutimage">
-                            <img src={image} alt=""/>
-                        </div>
-                        <div class="membername">
-                            <h3>{name}</h3>
-                        </div><br/>
-                        <p>{dept}</p> 
-                        <p>{college}</p> 
-                        <p>Mobile Number: {mobile}</p>
-                        <p>Email Id: {email}</p>
-                        <br/>
-                        <div class="facebook">
-                            <a href={fbLink}>
-                                <img src={faceboookImg} alt="Example"/>
-                              </a>
-                            <a href={ldLink}>
-                                <img src={linkdinImg} alt="Example"/>
-                              </a>
-                        </div>
-
-                    </div>
+            <div class="content">
+                <div class="aboutimage">
+                    <img src={image} alt="" />
                 </div>
-     );
+                <div class="membername">
+                    <h3>{name}</h3>
+                </div>
+                <br />
+                <p>{dept}</p>
+                <p>{college}</p>
+                <p>Mobile Number: {mobile}</p>
+                <p>Email Id: {email}</p>
+                <br />
+                <div class="facebook">
+                    <a href={fbLink}>
+                        <img src={faceboookImg} alt="Example" />
+                    </a>
+                    <a href={ldLink}>
+                        <img src={linkdinImg} alt="Example" />
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Member;
