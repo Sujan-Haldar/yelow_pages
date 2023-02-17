f5818a3b1c0b6388394de162e6e2d73fdd3dbaa2
ardhendu214 <ardhendudhua@gmail.com>
2023-02-17 13:36:28 +0530

    commit

--- client/src/components/FooterSection.js
+++ client/src/components/FooterSection.js
@@ -25,11 +25,11 @@ function FooterSection() {
                 </div>
                 <div className="box">
                     <h3>contact info</h3>
-                    <Link to="https://wa.me/918637546585">
+                    <Link to="https://wa.me/918637546585" target={"_blank"}>
                         {" "}
                         <i className="fas fa-phone"></i> +91 8637546585{" "}
                     </Link>
-                    <Link to="https://wa.me/919564714174">
+                    <Link to="https://wa.me/919564714174" target={"_blank"}>
                         {" "}
                         <i className="fas fa-phone"></i> +91 9564714174{" "}
                     </Link>
